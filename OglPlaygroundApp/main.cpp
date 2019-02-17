cb1b9f87fd0683113072fc398c84e90dea68f717
lukkio88 <luca.gagliano.1988@gmail.com>
2019-02-17 17:45:06 +0000

    Small change

--- OglPlaygroundApp/main.cpp
+++ OglPlaygroundApp/main.cpp
@@ -25,17 +25,29 @@ int main(void)
 		std::cout << "Error!" << std::endl;
 	}
 
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float position[6] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),position,GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float),0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
         /* Swap front and back buffers */
