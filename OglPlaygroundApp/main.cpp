eed53602e39354f18a2a183f4c54c95efe840ad0
lukkio88 <luca.gagliano.1988@gmail.com>
2019-02-18 12:02:32 +0000

    Using index buffer to render a square

--- OglPlaygroundApp/main.cpp
+++ OglPlaygroundApp/main.cpp
@@ -105,19 +105,29 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float position[6] = {
+	float position[8] = {
 		-0.5f, -0.5f,
-		0.0f, 0.5f,
-		0.5f, -0.5f
+		0.5f, -0.5f,
+		0.5f, 0.5f,
+		-0.5f,0.5f
 	};
 
-	unsigned int buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),position,GL_STATIC_DRAW);
+	unsigned int idx[6]{
+		0,1,2,2,3,0
+	};
+
+	unsigned int position_buffer;
+	glGenBuffers(1, &position_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float),position,GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float),0);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), idx, GL_STATIC_DRAW);
 
 	ShaderProgramsSrcs programsSrc = readShaderSrcs("../Basic.shader");
 	unsigned int program = createShader(programsSrc);
@@ -129,7 +139,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); //Why the nullptr?
 
 
         /* Swap front and back buffers */
