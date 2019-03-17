6d293cdf4d05b371c151a5d58eced34a50f406e2
lukkio88 <luca.gagliano.1988@gmail.com>
2019-03-17 18:11:32 +0000

    Small change in main, added a picture for debugging

--- OglPlaygroundApp/main.cpp
+++ OglPlaygroundApp/main.cpp
@@ -83,7 +83,7 @@ int main(void)
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.0f, 0.3f, 0.8f, 1.0f);
 
-		Texture texture("layla.png");
+		Texture texture("real-mayer.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
 
