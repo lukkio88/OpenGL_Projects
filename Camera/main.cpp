a14a62ba2b6a0c9b98dd4d48524fbd9a24c83f72
lukkio88 <luca.gagliano.1988@gmail.com>
2020-11-15 14:42:20 +0000

    model-view-projection simple code implemented

--- Camera/main.cpp
+++ Camera/main.cpp
@@ -1,4 +1,4 @@
-#include <glad/glad.h>
+#include<glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <glm/glm.hpp>
@@ -21,31 +21,30 @@ void processInput(GLFWwindow* window)
 const char* vertexShaderSource =
 "#version 330 core\n"
 "layout(location = 0) in vec3 aPos;\n"
-"layout(location = 1) in vec3 aColor;\n"
-"layout(location = 2) in vec2 aTexCoord;\n"
-"out vec3 ourColor;\n"
+"layout(location = 1) in vec2 aTexCoord;\n"
 "out vec2 TexCoord;\n"
-"uniform mat4 transform;\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = transform*vec4(aPos, 1.0);\n"
-"	ourColor = aColor;\n"
-"	TexCoord = aTexCoord;\n"
-"}\n";
+"	gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
+"	TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+"};\n";
 
 const char* fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
 "in vec2 TexCoord;\n"
-"uniform sampler2D ourTexture;\n"
+"uniform sampler2D inputTexture;\n"
 "void main()\n"
 "{\n"
-"	FragColor = texture(ourTexture, TexCoord);\n"
+"	FragColor = texture(inputTexture, TexCoord);\n"
 "}\n";
 
 int main()
 {
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -97,17 +96,9 @@ int main()
 	}
 
 	stbi_image_free(data);
-
 	//end texture
 
-	//Transformation
-	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-	trans = glm::scale(trans, glm::vec3(1.0, 1.0, 1.0));
-	//End transformation
-
 	//Shader section
-
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
@@ -153,10 +144,10 @@ int main()
 	//Buffer section
 	float vertices[] = {
 		// positions // colors // texture coords
-		0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-		-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // top left
+		0.5f, 0.5f, 0.0f, 1.0f, 1.0f, // top right
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f,  0.0f, 1.0f // top left
 	};
 
 
@@ -178,24 +169,18 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
 		(void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
 		(void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-		(void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	system("pwd");
-
 	while (!glfwWindowShouldClose(window))
 	{
-
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -206,9 +191,34 @@ int main()
 
 		glUseProgram(shaderProgram);
 
-		//Does the uniform in this case need to be set here?
-		unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+		// camera/view transformation
+		glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+		GLuint projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projection");
+		glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projection));
+
+		GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "view");
+		glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(view));
+
+		// render boxes
+		glBindVertexArray(VAO);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			
+			GLuint modelMatrixLocation = glGetUniformLocation(shaderProgram, "model");
+			glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glBindVertexArray(VAO); //This isn't necessary
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
