7d0a319f8608efacd370d26cd688f08f52e6bfe0
lukkio88 <luca.gagliano.1988@gmail.com>
2020-11-17 21:00:57 +0000

    Debugged colors app

--- Colors/main.cpp
+++ Colors/main.cpp
@@ -18,7 +18,7 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
-const char* vertexShaderSource =
+const char* vertexShaderSource = //same for light
 "#version 330 core\n"
 "layout(location = 0) in vec3 aPos;\n"
 "uniform mat4 model;\n"
@@ -39,9 +39,25 @@ const char* fragmentShaderSource =
 "	FragColor = vec4(lightColor * objectColor, 1.0);\n"
 "}\n";
 
+const char* fragmentShaderLightSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0);\n"
+"}\n";
+
 const char* vertexShaderError = "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n";
 const char* fragmentShaderError = "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n";
 const char* shaderProgramError = "ERROR::SHADER::PROGRAM::LINKING_FAILED\n";
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 bool checkShaderError(int success, int shaderId, const char* shaderError)
 {
@@ -52,17 +68,7 @@ bool checkShaderError(int success, int shaderId, const char* shaderError)
 		std::cout << shaderError <<
 			infoLog << std::endl;
 	}
-}
-
-bool checkShaderProgramError(int success, int shaderProgram, const char* shaderError)
-{
-	if (!success)
-	{
-		char infoLog[512];
-		glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << shaderError <<
-			infoLog << std::endl;
-	}
+	return success;
 }
 
 //As we require two shaders here I define some utility functions, a class would be better for so many reasons
@@ -75,7 +81,6 @@ int createAndCompileShader(const char* shaderSourceCode, unsigned int& shaderId,
 	glCompileShader(shaderId);
 
 	int success;
-	char infoLog[512];
 	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
 	return success;
 }
@@ -121,31 +126,49 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//Shader section
-	unsigned int vertexShader;
+	unsigned int vertexShader = 0, vertexShaderLight = 0;
 	checkShaderError(
 		createAndCompileShader(vertexShaderSource, vertexShader, GL_VERTEX_SHADER), 
 		vertexShader, 
 		vertexShaderError
 	);
 
-	unsigned int fragmentShader;
+	checkShaderError(
+		createAndCompileShader(vertexShaderSource, vertexShaderLight, GL_VERTEX_SHADER),
+		vertexShaderLight,
+		vertexShaderError
+	);
+
+	unsigned int fragmentShader = 0, fragmentShaderLight = 0;
 	checkShaderError(
 		createAndCompileShader(fragmentShaderSource, fragmentShader, GL_FRAGMENT_SHADER),
 		fragmentShader,
 		fragmentShaderError
 	);
 
-	unsigned int shaderProgram;
-	checkShaderProgramError(
+	checkShaderError(
+		createAndCompileShader(fragmentShaderLightSource, fragmentShaderLight, GL_FRAGMENT_SHADER),
+		fragmentShaderLight,
+		fragmentShaderError
+	);
+
+	unsigned int shaderProgram = 0, shaderProgramLight = 0;
+	checkShaderError(
 		createAndLinkShaderProgram(vertexShader, fragmentShader, shaderProgram),
 		shaderProgram,
 		shaderProgramError
 	);
 
+	checkShaderError(
+		createAndLinkShaderProgram(vertexShaderLight, fragmentShaderLight, shaderProgramLight),
+		shaderProgramLight,
+		shaderProgramError
+	);
+
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-
-	//check error for linking and compilation
+	glDeleteShader(vertexShaderLight);
+	glDeleteShader(fragmentShaderLight);
 
 	glUseProgram(shaderProgram); //Not sure what to do with this
 
@@ -217,12 +240,20 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// lighting position
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 
 		glUseProgram(shaderProgram);
 
@@ -235,36 +266,46 @@ int main()
 		glUniform3fv(lightColorLocation, 1, glm::value_ptr(lightColor));
 
 		// camera/view transformation
+		GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "view");
+		GLuint projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projection");
+		GLuint modelMatrixLocation = glGetUniformLocation(shaderProgram, "model");
+
+
 		glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
 		float radius = 10.0f;
 		float camX = sin(glfwGetTime()) * radius;
 		float camZ = cos(glfwGetTime()) * radius;
 		view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
-		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
-		GLuint projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projection");
-		glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projection));
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-		GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "view");
+		glm::mat4 model = glm::mat4(1.0f);
+		float angle = 0.0f;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
 		glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(model));
 
-		// render boxes
+		// render the cube
 		glBindVertexArray(VAO);
-		for (unsigned int i = 0; i < 10; i++)
-		{
-			// calculate the model matrix for each object and pass it to shader before drawing
-			glm::mat4 model = glm::mat4(1.0f);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-			GLuint modelMatrixLocation = glGetUniformLocation(shaderProgram, "model");
-			glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(model));
+		// also draw the lamp object
+		glUseProgram(shaderProgramLight);
+		projectionMatrixLocation = glGetUniformLocation(shaderProgramLight, "projection");
+		viewMatrixLocation = glGetUniformLocation(shaderProgramLight,"view");
+		modelMatrixLocation = glGetUniformLocation(shaderProgramLight, "model");
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		
+		glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(model));
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
-
-		glBindVertexArray(VAO); //This isn't necessary
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(lightVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
